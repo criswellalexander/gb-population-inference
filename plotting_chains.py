e90a0bc96aa843f1a703617c93ff41a08abb928f
Robert Rosati <robert.j.rosati@nasa.gov>
2024-05-01 13:15:33 -0500

    nicer plots

--- plotting_chains.py
+++ plotting_chains.py
@@ -6,7 +6,7 @@ import os
 import pandas as pd
 
 def plot_chains(Nresolved : int, chains, pop_param_labels : list[str] = [r'$\alpha$', r'$\beta$'], truths = [], plotfolder : str = 
-pathlib.Path(__file__).parent.absolute() ):
+os.path.join(pathlib.Path(__file__).parent.absolute(),'plots') ):
     labels = []
     for i in range(Nresolved):
         labels.extend([f'$A_{{{i}}}$', f'$f_{{{i}}}$', f'$\\phi_{{{i}}}$'])
@@ -14,15 +14,19 @@ pathlib.Path(__file__).parent.absolute() ):
     df = pd.DataFrame(chains, columns = labels)
     c = ChainConsumer()
     c.add_chain(Chain(name='samples',samples=df))
-    c.add_truth(Truth(location={l: t for l,t in zip(labels,truths)}))
-    fig = c.plotter.plot(filename=os.path.join(plotfolder,'chains.pdf'))
+    c.add_truth(Truth(location={l: t for l,t in zip(labels,truths)}, line_width=2.0))
+    if not os.path.exists(plotfolder):
+        os.mkdir(plotfolder)
+    fig = c.plotter.plot(filename=os.path.join(plotfolder,'full_corner.pdf'))
+    fig = c.plotter.plot(columns = labels[-len(pop_param_labels):], filename=os.path.join(plotfolder,'pop_params.pdf'))
 
 
 
-from scipy.stats import multivariate_normal
-Nresolved = 3
+if __name__ == '__main__':
+    from scipy.stats import multivariate_normal
+    Nresolved = 3
 
-means = [1e-20,1e-3,0.5]*Nresolved + [1.0,1.0]
-cov = np.diag(1e-4*np.ones_like(means))
-fake_chains = multivariate_normal(mean=means,cov=cov,allow_singular = False).rvs(10000)
-plot_chains(Nresolved, fake_chains, truths = means)
+    means = [1e-20,1e-3,0.5]*Nresolved + [1.0,1.0]
+    cov = np.diag(1e-4*np.ones_like(means))
+    fake_chains = multivariate_normal(mean=means,cov=cov,allow_singular = False).rvs(10000)
+    plot_chains(Nresolved, fake_chains, truths = means)
