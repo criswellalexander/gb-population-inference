fixing mask generation + adding Fij calculation

foreground.py
@@ -11,9 +11,10 @@ xp = np
 class foreground():
 
 
-    def __init__(self, fbins, nresovled=5, snr_thresh=10):
+    def __init__(self, noisePSD, fbins, nresovled=5, snr_thresh=10):
         self.snr_thresh = snr_thresh
         self.fbins = fbins
+        self.noisePSD = noisePSD
         self.delf = fbins[1] - fbins[0]
         self.fmins_ub = fbins + 0.5*self.delf
         self.fmins_lb = fbins - 0.5*self.delf
@@ -22,6 +23,8 @@ class foreground():
         self.mc_min, self.mc_max = 0.5, 1.5 ## in solar masses
         self.r_min, self.r_max =  3.34e-4, 1.3e-2 ## in AU
         self.d_min, self.d_max = 1, 50 ## in kpc
+        self.ndraw = int(1e4)
+
 
         self.fmask, self.base_population = self._base_population()
 
@@ -30,17 +33,18 @@ class foreground():
 
         wts = self.get_pop_wts(alpha, beta)
 
-        wts2 = wts[self.fmask]
-
         nunresolved = Ntot - self.nresolved
 
-        import pdb; pdb. set_trace()
 
+        Fij = self.base_population['A'] / (self.noisePSD[:, None] + \
+                                           jnp.cumsum(wts * self.base_population['A'], axis=1))
+
+        import pdb; pdb.set_trace()
 
         prop_factor = Ntot * jnp.sum()
 
-        prop_factor = Ntot * self.fmask.sum(axis=1) / self.fmax.sum()
-        
+        prop_factor = Ntot * self.fmask.sum(axis=1) 
+
 
 
         return psd
@@ -54,11 +58,16 @@ class foreground():
         jnp.log(jnp.abs(jnp.power(self.r_max, beta + 1) - jnp.power(self.r_min, beta + 1)))
 
         log_p_d = jnp.log(2) + jnp.log(self.base_population['d']) - \
-        jnp.log(jnp.power(self.d_max, 2) - jnp.power(self.d_min, 2)) 
+        jnp.log(jnp.power(self.d_max, 2) - jnp.power(self.d_min, 2))
 
-        wts =  jnp.exp(log_p_mc + log_p_r + log_p_d - self.base_population['log_priors']) 
+        wts =  jnp.exp(log_p_mc + log_p_r + log_p_d - self.base_population['log_priors'])
+        
+        wts = np.where(self.base_population['mc'] == 0, 0, wts)
+        wts = np.where(self.base_population['r'] == 0, 0, wts)
+        wts = np.where(self.base_population['d'] == 0, 0, wts)
 
-        wts = wts / jnp.sum(wts) * self.base_population['r'].size
+
+        wts = wts / jnp.sum(wts)
 
 
         return wts
@@ -67,16 +76,15 @@ class foreground():
 
     def _base_population(self):
 
-        ndraw = int(1e5)
-        mc_draw = np.random.uniform(low=self.mc_min, high=self.mc_max, size=ndraw) 
-        r_draw = np.random.uniform(low=self.r_min, high=self.r_max, size=ndraw)
-        d_draw = np.sqrt(self.d_min**2 + np.random.uniform(size=ndraw) * \
+        mc_draw = np.random.uniform(low=self.mc_min, high=self.mc_max, size=self.ndraw)
+        r_draw = np.random.uniform(low=self.r_min, high=self.r_max, size=self.ndraw)
+        d_draw = np.sqrt(self.d_min**2 + np.random.uniform(size=self.ndraw) * \
                          (self.d_max**2 - self.d_min**2) )
 
         f_draw = self.calc_freqs(mc_draw, r_draw).value
 
 
-        A_draw = self.calc_amplitudes(mc_draw, f_draw, d_draw)
+        A_draw = self.calc_amplitudes(mc_draw, f_draw, d_draw).value
 
 
         base_population = {}
@@ -89,25 +97,40 @@ class foreground():
         r_draw = r_draw[sort_mask]
         d_draw = d_draw[sort_mask]
 
-        base_population['mc'] = mc_draw
-        base_population['r'] = r_draw
-        base_population['d'] = d_draw
-        base_population['A'] = A_draw.value
-        base_population['f'] = f_draw
-       
-        base_population['log_priors'] =  - np.log(self.mc_max - self.mc_min) - \
-                                        np.log(self.r_max - self.r_min) - \
-                                        np.log(2) + np.log(d_draw) - \
-                                        np.log(self.d_max**2 - self.d_min**2) 
-
         # Masking by freq bin
-        fmask = np.zeros((len(fbins),ndraw), dtype='int')
+        fmask = np.zeros((len(fbins), self.ndraw), dtype='int')
+        mc_sorted = np.zeros((len(fbins),self.ndraw))
+        r_sorted = np.zeros((len(fbins),self.ndraw))
+        d_sorted = np.zeros((len(fbins),self.ndraw))
+        A_sorted = np.zeros((len(fbins),self.ndraw))
+        f_sorted = np.zeros((len(fbins),self.ndraw))
+
         for i, fbin in enumerate(fbins):
             fub = self.fmins_ub[i]
             flb = self.fmins_lb[i]
             mask = (f_draw >= flb)*(f_draw < fub)
+
+            if mask.sum() > 0:
+                mc_sorted[i, -mask.sum():] = mc_draw[mask]
+                r_sorted[i, -mask.sum():] = r_draw[mask]
+                d_sorted[i, -mask.sum():] = d_draw[mask]
+                A_sorted[i, -mask.sum():] = A_draw[mask]
+                f_sorted[i, -mask.sum():] = f_draw[mask]
+
             fmask[i] = mask
             #print(base_population['A'][mask])
+
+        base_population['mc'] = mc_sorted
+        base_population['r'] = r_sorted
+        base_population['d'] = d_sorted
+        base_population['A'] = A_sorted
+        base_population['f'] = f_sorted
+
+        base_population['log_priors'] =  - np.log(self.mc_max - self.mc_min) - \
+                                        np.log(self.r_max - self.r_min) - \
+                                        np.log(2) + np.log(d_sorted) - \
+                                        np.log(self.d_max**2 - self.d_min**2)
+
         return fmask, base_population
 
     def calc_freqs(self, mc, radius):
@@ -130,9 +153,10 @@ class foreground():
 
 if __name__ == "__main__":
     #fbins = (10**(np.linspace(-5,-2,10)))/u.s
-    fbins = np.linspace(1e-4, 1e-2, 50) 
-    fg = foreground(fbins)
-    fg.psd(-2, -1.5, 2000)
+    fbins = np.linspace(1e-4, 1e-2, 50)
+    noisepsd = 1e-20 * np.ones(fbins.shape)    
+    fg = foreground(noisepsd, fbins)
+    fg.psd(-2, -1.5, 500)
 
 
 